cd169ad2d848db49faec3edb0129f73317df0384
DaMolks <108116237+DaMolks@users.noreply.github.com>
2024-12-30 15:29:50 +0100

    Script d'installation simplifié

--- install_and_run.bat
+++ install_and_run.bat
@@ -2,41 +2,21 @@
 
 echo Installation du projet Kotlin AI IDE
 
-:: Verification Node.js
-node --version >nul 2>&1
-if errorlevel 1 (
-    echo Node.js non installe
-    exit /b 1
-)
+npm install -g yarn
 
-:: Verification Yarn
-yarn --version >nul 2>&1
-if errorlevel 1 (
-    echo Yarn non installe
-    npm install -g yarn
-)
-
-:: Installation des dependances
-echo Installation des dependances...
-call yarn install
+yarn install
 
 cd backend
-call yarn install
+yarn install
 cd ..
 
-:: Build du projet
-echo Construction du projet...
-call yarn build
+yarn build
 
 cd backend
-call yarn build
+yarn build
 cd ..
-
-:: Demarrage des services
-echo Demarrage des services...
 
 start "Backend" cmd /c "cd backend && yarn dev"
 start "Frontend" cmd /c "yarn start"
 
-echo Services demarres !
 pause
