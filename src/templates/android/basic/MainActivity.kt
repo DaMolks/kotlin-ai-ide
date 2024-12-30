bffa131df630758c3bfadba1d577b1db9c7cac21
DaMolks <108116237+DaMolks@users.noreply.github.com>
2024-12-30 14:34:37 +0100

    Add MainActivity template

---
+++ src/templates/android/basic/MainActivity.kt
@@ -0,0 +1,11 @@
+package {packageName}
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
